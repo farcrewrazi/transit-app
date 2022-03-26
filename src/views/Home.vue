04381b35b64a7531e00c779ba6bb3a1724c73d15
farcrew razi <farcrewrz@gmail.com>
2022-03-26 10:32:14 +0800

    Initiated first logo page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,16 @@
 <template>
   <div id="homepage">
-    Yezza this is home
+    <div class="flex h-screen items-center justify-center">
+
+      <!-- Card -->
+      <card
+        class="rounded-2xl shadow hover:-translate-y-1 hover:shadow-2xl delay-75 duration-100 bg-white cursor-pointer">
+
+        <img class="rounded-2xl" src="./../assets/img/transit_standard.png" alt="">
+
+      </card>
+
+    </div>
   </div>
 </template>
 
