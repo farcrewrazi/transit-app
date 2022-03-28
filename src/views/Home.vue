387e2fce55181c23470974e8aef5b0bfedc87da5
farcrew razi <farcrewrz@gmail.com>
2022-03-28 22:48:00 +0800

    Changed logo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,7 +6,7 @@
       <card
         class="rounded-2xl shadow hover:-translate-y-1 hover:shadow-2xl delay-75 duration-100 bg-white cursor-pointer">
 
-        <img class="rounded-2xl" src="./../assets/img/transit_standard.png" alt="">
+        <img class="rounded-2xl" src="./../assets/img/transit_standard_3.png" alt="">
 
       </card>
 
